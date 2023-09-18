3adf8d386a3a0ba35176f99d71c7e6fef1e709b7
alexsmirn <alexsmirnoffprogramming@gmail.com>
2023-09-18 10:12:16 +0300

    8kyu-Multiply

---
+++ codewars/8kyu-Multiply.js
@@ -0,0 +1,13 @@
+//🤔DESCRIPTION:
+// This code does not execute properly. Try to figure out why.
+// function multiply(a, b){
+//   a * b
+// }
+
+//✅SOLUTION:
+
+function multiply(a, b){
+    return a * b
+}
+
+console.log(multiply(10, 5))
